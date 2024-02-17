efaa0b18f9391b68cc294d3a246f340dfa635734
Omer <dashhawk96@gmail.com>
2024-02-17 12:39:27 +0200

    day 8 pary 2 optimized

--- src/2023/day8.ts
+++ src/2023/day8.ts
@@ -2,15 +2,13 @@ import { getInput, registerFunc } from "../utils";
 
 const day8Part1 = () => {
 	const data = getInput();
-	const instructions = data.splice(0, 2)[0];
+	const turns = data.splice(0, 2)[0];
 	const nodes = getNodes(data);
 	let counter = 0;
 	let currNode = "AAA";
-	while (currNode !== "ZZZ" || counter % instructions.length !== 0) {
+	while (currNode !== "ZZZ" || counter % turns.length !== 0) {
 		currNode =
-			nodes[currNode][
-				instructions.charAt(counter % instructions.length) === "L" ? 0 : 1
-			];
+			nodes[currNode][turns.charAt(counter % turns.length) === "L" ? 0 : 1];
 		counter++;
 	}
 	return counter;
@@ -22,15 +20,16 @@ const day8Part2 = () => {
 	const nodes = getNodes(data);
 	let counter = 0;
 	let currNodes = Object.keys(nodes).filter((n) => n.endsWith("A"));
-	while (
-		!currNodes.every((n) => n.endsWith("Z")) ||
-		counter % turns.length !== 0
-	) {
-		const turn = turns.charAt(counter % turns.length) === "L" ? 0 : 1;
-		currNodes = currNodes.map((n) => nodes[n][turn]);
-		counter++;
-	}
-	return counter;
+	const counters: number[] = [];
+	currNodes.forEach((node) => {
+		while (!node.endsWith("Z") || counter % turns.length !== 0) {
+			node = nodes[node][turns.charAt(counter % turns.length) === "L" ? 0 : 1];
+			counter++;
+		}
+		counters.push(counter);
+		counter = 0;
+	});
+	return lcmOfArray(counters);
 };
 
 export const registerDay8 = () => {
@@ -46,4 +45,19 @@ const getNodes = (data: string[]) => {
 		nodes[split[0].trim()] = split[1].split(",").map((s) => s.trim());
 	});
 	return nodes;
+};
+
+const gcd = (a: number, b: number) => {
+	if (b === 0) return a;
+	return gcd(b, a % b);
+};
+
+const lcm = (a: number, b: number) => (a * b) / gcd(a, b);
+
+const lcmOfArray = (numbers: number[]) => {
+	let result = 1;
+	for (let i = 0; i < numbers.length; i++) {
+		result = lcm(result, numbers[i]);
+	}
+	return result;
 };
