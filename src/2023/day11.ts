f374faad51bcedabf25a0fb12be6c549d8a68036
Deserar Omer <omar1109@elta.co.il>
2024-02-25 08:18:08 +0200

    day 11 part 2 done

--- src/2023/day11.ts
+++ src/2023/day11.ts
@@ -1,38 +1,73 @@
 import { getInput, registerFunc } from "../utils";
 
 const day11Part1 = () => {
-	const universe = getInput().map((s) => s.split(""));
-	const addIndexes: number[] = [];
-	for (let i = 0; i < universe.length; i++) {
-		if (!universe[i].includes("#")) addIndexes.unshift(i);
-	}
-	addIndexes.forEach((i) => universe.splice(i, 0, universe[i]));
-	addIndexes.length = 0;
-	for (let i = 0; i < universe[0].length; i++) {
-		if (!universe.map((arr) => arr[i]).includes("#")) addIndexes.unshift(i);
-	}
-	addIndexes.forEach((i) => universe.forEach((arr) => arr.splice(i, 0, ".")));
-	const galaxyCoords: number[][] = [];
-	for (let i = 0; i < universe.length; i++) {
-		for (let j = 0; j < universe[0].length; j++) {
-			if (universe[i][j] === "#") galaxyCoords.push([i, j]);
-		}
-	}
-	let counter = 0;
-	for (let i = 0; i < galaxyCoords.length; i++) {
-		for (let j = i + 1; j < galaxyCoords.length; j++) {
-			const [xA, yA] = galaxyCoords[i];
-			const [xB, yB] = galaxyCoords[j];
-			counter += Math.abs(xA - xB);
-			counter += Math.abs(yA - yB);
-		}
-	}
-	return counter;
+  const universe = getInput().map((s) => s.split(""));
+  const addIndexes: number[] = [];
+  for (let i = 0; i < universe.length; i++) {
+    if (!universe[i].includes("#")) addIndexes.unshift(i);
+  }
+  addIndexes.forEach((i) => universe.splice(i, 0, universe[i]));
+  addIndexes.length = 0;
+  for (let i = 0; i < universe[0].length; i++) {
+    if (!universe.map((arr) => arr[i]).includes("#")) addIndexes.unshift(i);
+  }
+  addIndexes.forEach((i) => universe.forEach((arr) => arr.splice(i, 0, ".")));
+  const galaxyCoords: number[][] = [];
+  for (let i = 0; i < universe.length; i++) {
+    for (let j = 0; j < universe[0].length; j++) {
+      if (universe[i][j] === "#") galaxyCoords.push([i, j]);
+    }
+  }
+  let counter = 0;
+  for (let i = 0; i < galaxyCoords.length; i++) {
+    for (let j = i + 1; j < galaxyCoords.length; j++) {
+      const [xA, yA] = galaxyCoords[i];
+      const [xB, yB] = galaxyCoords[j];
+      counter += Math.abs(xA - xB);
+      counter += Math.abs(yA - yB);
+    }
+  }
+  return counter;
 };
 
-const day11Part2 = () => {};
+const day11Part2 = () => {
+  const universe = getInput().map((s) => s.split(""));
+  const emptyIndexes: { horizontal: number[]; vertical: number[] } = {
+    horizontal: [],
+    vertical: [],
+  };
+  for (let i = 0; i < universe.length; i++) {
+    if (!universe[i].includes("#")) emptyIndexes.horizontal.push(i);
+  }
+  for (let i = 0; i < universe[0].length; i++) {
+    if (!universe.map((arr) => arr[i]).includes("#"))
+      emptyIndexes.vertical.push(i);
+  }
+  const galaxyCoords: number[][] = [];
+  for (let i = 0; i < universe.length; i++) {
+    for (let j = 0; j < universe[0].length; j++) {
+      if (universe[i][j] === "#") galaxyCoords.push([i, j]);
+    }
+  }
+  let counter = 0;
+  for (let i = 0; i < galaxyCoords.length; i++) {
+    for (let j = i + 1; j < galaxyCoords.length; j++) {
+      let [xA, yA] = galaxyCoords[i];
+      let [xB, yB] = galaxyCoords[j];
+      while (xA !== xB) {
+        xA += xA < xB ? 1 : -1;
+        counter += emptyIndexes.horizontal.includes(xA) ? 1000000 : 1;
+      }
+      while (yA !== yB) {
+        yA += yA < yB ? 1 : -1;
+        counter += emptyIndexes.vertical.includes(yA) ? 1000000 : 1;
+      }
+    }
+  }
+  return counter;
+};
 
 export const registerDay11 = () => {
-	registerFunc(2023, 11, 1, day11Part1);
-	registerFunc(2023, 11, 2, day11Part2);
+  registerFunc(2023, 11, 1, day11Part1);
+  registerFunc(2023, 11, 2, day11Part2);
 };
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { initFunctionDB, runFunc } from "./utils";
 
 const YEAR = 2023;
 const DAY = 11;
-const PART = 1;
+const PART = 2;
 
 const main = async () => {
-	initFunctionDB();
-	console.log(
-		`Year:     ${YEAR}\nQuestion: ${DAY}\nPart:     ${PART}\nCalculating...`
-	);
-	console.log(await runFunc(YEAR, DAY, PART));
+  initFunctionDB();
+  console.log(
+    `Year:     ${YEAR}\nQuestion: ${DAY}\nPart:     ${PART}\nCalculating...`
+  );
+  console.log(await runFunc(YEAR, DAY, PART));
 };
 
 main();
